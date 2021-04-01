4ccef99beb442ec2fbff4d17b64c23fd2048d490
Archie Smyth <archiesmyth26@googlemail.com>
2021-04-01 12:24:40 +0100

    rename redux provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import "tailwindcss/tailwind.css";
 import type { AppProps } from 'next/app'
+
 import Layout from '../components/Layout';
-import { Provider } from 'react-redux';
+import { Provider as ReduxContext } from 'react-redux';
 import { useStore } from '../redux/store';
 import { ToastContainer } from 'react-toastify'
 import { ThemeProvider } from 'next-themes'
@@ -9,15 +10,17 @@ import { ThemeProvider } from 'next-themes'
 export default function App({ Component, pageProps }: AppProps) {
   const store = useStore(pageProps.initialReduxState)
 
-  
+
   return (
-    <Provider store={store}>
+
+    <ReduxContext store={store}>
       <ThemeProvider attribute="class">
         <Layout>
           <Component {...pageProps} />
           <ToastContainer />
         </Layout>
       </ThemeProvider>
-    </Provider>
+    </ReduxContext>
+
   )
 }
