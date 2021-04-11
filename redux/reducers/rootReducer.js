60287833453d31c649d7adc6a1f1c4603c328f17
Archie Smyth <archiesmyth26@googlemail.com>
2021-04-11 17:15:07 +0100

    App config. Removal of redux

--- redux/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import accountReducer from './accountReducer';
-
-import {combineReducers} from 'redux';
-
-const rootReducer = combineReducers({
-    account: accountReducer
-});
-
-export default rootReducer;
